4bdccd8d015b2dab8d43a986c62f9036aa026163
Boris Klco <klco@pm.me>
2023-04-13 12:44:51 +0000

    login-controller ✔

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -3,6 +3,47 @@ import express from "express";
 import { createUser, getUserByEmail } from "../db/users";
 import { authentication, random } from "../helpers/index";
 
+export const login = async (req: express.Request, res: express.Response) => {
+  try {
+    const { password, email } = req.body;
+
+    if (!password || !email) {
+      return res.sendStatus(400);
+    }
+
+    const user = await getUserByEmail(email).select(
+      "+authentication.salt +authentication.password"
+    );
+
+    if (!user) {
+      return res.sendStatus(400);
+    }
+
+    const expectedHash = authentication(user.authentication.salt, password);
+
+    if (user.authentication.password != expectedHash) {
+      return res.sendStatus(403);
+    }
+
+    const salt = random();
+    user.authentication.sessionToken = authentication(
+      salt,
+      user._id.toString()
+    );
+
+    await user.save();
+
+    res.cookie("node-restapi-auth", user.authentication.sessionToken, {
+      domain: "localhost",
+    });
+
+    return res.status(200).json(user).end();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
 export const register = async (req: express.Request, res: express.Response) => {
   try {
     const { email, password, username } = req.body;
